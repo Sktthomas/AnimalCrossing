43e78e34722065cfa8be49ab6fde9952c65047df
Thomas Nielse <skt.thomas@gmail.com>
2019-12-16 12:59:36 +0100

    Final commit

--- AnimalCrossing/Models/DbInitializer.cs
+++ AnimalCrossing/Models/DbInitializer.cs
@@ -37,10 +37,11 @@ namespace AnimalCrossing.Models
 
                     var cats = new Cat[]
                     {
-                new Cat{Name="Fat Cat", BirthDate=new DateTime(2019, 9, 23), Description="Fat as fuck lil kitty", Gender=Gender.Male, SpeciesId=speciesType[0].SpeciesId}, //Notice that you access the Gender enum, then choose your gender within that
-                new Cat{Name="Spunky", BirthDate=new DateTime(2019, 9, 23), Description="Got a lotta spunk", Gender=Gender.Male, SpeciesId=speciesType[1].SpeciesId}, //Notice the speciesType allows us to access the first (0th) index of species, instead of the SpeciesID
-                new Cat{Name="TipTop", BirthDate=new DateTime(2019, 9, 23), Description="Best cat", Gender=Gender.Female, SpeciesId=speciesType[2].SpeciesId},
-                new Cat{Name="Somebody", BirthDate=new DateTime(2019, 9, 23), Description="Fat as fuck lil kitty", Gender=Gender.Male, SpeciesId=speciesType[3].SpeciesId}
+                new Cat{Name="Fat Cat", BirthDate=new DateTime(2019, 9, 23), Description="Fat as fuck lil kitty", Gender=Gender.Male, SpeciesId=speciesType[0].SpeciesId, ProfilePicture="https://pbs.twimg.com/media/DtBuHPeWkAAu9q8.jpg"}, //Notice that you access the Gender enum, then choose your gender within that
+                new Cat{Name="Spunky", BirthDate=new DateTime(2019, 9, 23), Description="Got a lotta spunk", Gender=Gender.Male, SpeciesId=speciesType[1].SpeciesId, ProfilePicture="https://upload.wikimedia.org/wikipedia/en/8/87/Keyboard_cat.jpg"}, //Notice the speciesType allows us to access the first (0th) index of species, instead of the SpeciesID
+                new Cat{Name="TipTop", BirthDate=new DateTime(2019, 9, 23), Description="Best cat", Gender=Gender.Female, SpeciesId=speciesType[2].SpeciesId, ProfilePicture="https://i.kym-cdn.com/entries/icons/original/000/026/638/cat.jpg"},
+                new Cat{Name="Somebody", BirthDate=new DateTime(2019, 9, 23), Description="Just a cat", Gender=Gender.Male, SpeciesId=speciesType[3].SpeciesId, ProfilePicture="https://66.media.tumblr.com/31aca5977137e87fd301d79781c4263c/tumblr_inline_ofu86bnxV21r7k951_540.jpg"},
+                new Cat{Name="Pugio", BirthDate=new DateTime(2019, 12, 15), Description="A cat you used to know", Gender=Gender.Other, SpeciesId=speciesType[2].SpeciesId, ProfilePicture="https://hips.hearstapps.com/hmg-prod.s3.amazonaws.com/images/cute-cat-breeds-ragdoll-1568332272.jpg?crop=0.668xw:1.00xh;0.104xw,0&resize=480:*"}
                     };
                     foreach (Cat c in cats)
                     {
